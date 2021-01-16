c9ed04989f1da10d9cd074401b023ad506ba53ce
mromo <mromo@alumnosuls.cl>
2021-01-16 14:12:03 -0300

    Commit

--- Transcriptor_1/Celda.cpp
+++ Transcriptor_1/Celda.cpp
@@ -1,5 +1,27 @@
 #include "Celda.h"
 
-namespace celda {
+namespace Celda {
 
-}
+
+
+};
+
+/*
+class MyClass {       // The class
+public:             // Access specifier
+    int myNum;        // Attribute (int variable)
+    string myString;  // Attribute (string variable)
+};
+
+int main() {
+    MyClass myObj;  // Create an object of MyClass
+
+    // Access attributes and set values
+    myObj.myNum = 15;
+    myObj.myString = "Some text";
+
+    // Print attribute values
+    cout << myObj.myNum << "\n";
+    cout << myObj.myString;
+    return 0;
+}*/
---
+++ Transcriptor_1/Neurona.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+class  Neurona {
+	//metodo para inicializar los pesos, y todo eso
+	//metodo para empezar todo el ajetreo
+
+	//actualizar celda
+
+	//puerta olvido
+
+	//puerta de actualizacion
+
+	//puerta de salida
+	private:
+		std::vector<float> Ct;
+		std::vector<float> Ht;
+
+	public:
+		Neurona();
+
+
+};
+
+Neurona::Neurona() {
+
+}
+
